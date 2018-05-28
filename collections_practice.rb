c7d2215828b805b3f05445f19f5c068b01529153
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-28 04:12:21 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -62,7 +62,7 @@ def merge_data(kys, ar)
       kys.each do |hsh_chnk|
         hsh_chnk.each do |k, v|
           if v == first_key
-          new_hash[k] = first_key
+          new_hash = {k => first_key}
           data.each do |sec_key, sec_data|
             new_hash[sec_key] = sec_data
           end
